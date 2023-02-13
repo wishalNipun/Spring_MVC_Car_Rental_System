819cfd9512a4076af49b5263b4a47f1cee19bb74
wishalNipun <wishal1974@gmail.com>
2023-02-13 13:48:44 +0530

    content Update

---
+++ Spring_MVC_Car_Rental_System/FrontEnd/assets/controllers/DashBoardController.js
@@ -0,0 +1,26 @@
+$('#Home').click(function (){
+    // $('#navHome').css('display','block');
+    // $('#navCustomer').css('display','none');
+    // $('#navItem').css('display','none');
+    // $('#navPlaceOrder').css('display','none');
+    // $('#navOrderDetail').css('display','none');
+
+    $('#UserDashBoard').css('display','none');
+
+    $('#UserCreateAccount').css('display','none');
+
+});
+
+
+$('#Account').click(function (){
+    console.log("ssss")
+    // $('#navHome').css('display','block');
+    // $('#navCustomer').css('display','none');
+    // $('#navItem').css('display','none');
+    // $('#navPlaceOrder').css('display','none');
+    // $('#navOrderDetail').css('display','none');
+
+    $('#UserDashBoard').css('display','none');
+
+
+});
