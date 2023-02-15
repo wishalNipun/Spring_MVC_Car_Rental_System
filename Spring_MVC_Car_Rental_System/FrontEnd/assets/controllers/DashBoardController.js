40b155384f58724711fa01da3a47bfa00c2888a1
wishalNipun <wishal1974@gmail.com>
2023-02-15 12:29:11 +0530

    content updated

--- Spring_MVC_Car_Rental_System/FrontEnd/assets/controllers/DashBoardController.js
+++ Spring_MVC_Car_Rental_System/FrontEnd/assets/controllers/DashBoardController.js
@@ -1,17 +1,44 @@
 $('#UserDashBoard').css('display','block');
 
 $('#UserAccount').css('display','none');
+$('#UserLoginAccount').css('display','none');
+
+
 
 $('.Home').click(function (){
     $('#UserDashBoard').css('display','block');
 
+
     $('#UserAccount').css('display','none');
+    $('#UserLoginAccount').css('display','none');
+
+    $('#UserDashBoardHeadNav>ul>li>a').css('color','white');
+    $(' #UserDashBoardHeadNav>ul>li:nth-child(1)>a').css('color','black');
+    $('#UserDashBoardHeadNav>ul>li>a').css('font-weight','400');
+    $(' #UserDashBoardHeadNav>ul>li:nth-child(1)>a').css('font-weight','800');
+
 });
 
 
 $('.Account').click(function (){
     $('#UserAccount').css('display','block');
+    $('#UserAccountNav').css('display','block');
 
     $('#UserDashBoard').css('display','none');
+    $('#UserLoginAccount').css('display','none');
+
+    $('#UserDashBoardHeadNav>ul>li>a').css('color','black');
+    $('#UserDashBoardHeadNav>ul>li>a').css('font-weight','400');
+    $(' #UserDashBoardHeadNav>ul>li:nth-child(2)>a').css('font-weight','800');
+
+
+});
+
+$('#logHere').click(function (){
+    $('#UserLoginAccount').css('display','block');
+
+
+    $('#UserDashBoard').css('display','none');
+    $('#UserAccount').css('display','none');
 
 });
