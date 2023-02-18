46ec1080f106be89bb51ea175fd6f309cd5b629f
wishalNipun <wishal1974@gmail.com>
2023-02-18 19:52:20 +0530

    front end admin updated

---
+++ Spring_MVC_Car_Rental_System/FrontEnd/assets/controllers/AdminDashBoardController.js
@@ -0,0 +1,39 @@
+$('#adminDashBoard').css('display','block');
+
+$('#adminCustomer').css('display','none');
+
+
+$('.DashBoard').click(function (){
+    $('#adminDashBoard').css('display','block');
+
+    $('#adminCustomer').css('display','none');
+
+
+});
+
+
+$('.Customer').click(function (){
+    $('#adminCustomer').css('display','block');
+
+    $('#adminDashBoard').css('display','none');
+
+});
+
+$('.Drivers').click(function (){
+
+});
+
+$('.Reservation').click(function (){
+
+
+});
+
+$('.Cars').click(function (){
+
+
+
+});
+
+$('.Payment').click(function (){
+
+});
