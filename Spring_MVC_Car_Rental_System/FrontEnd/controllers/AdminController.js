46f8c22303fcd57d644f97742be28211fda5da96
wishalNipun <wishal1974@gmail.com>
2023-02-24 13:39:11 +0530

    content update

--- Spring_MVC_Car_Rental_System/FrontEnd/controllers/AdminController.js
+++ Spring_MVC_Car_Rental_System/FrontEnd/controllers/AdminController.js
@@ -31,95 +31,82 @@ function loadAllAdmins() {
 
 
                 var row = '<tr><td>' + admin.adminId + '</td><td>' +admin.email+ '</td><td>' + admin.username + '</td><td>' + admin.password + '</td></tr>';
-                $("#tblDriver").append(row);
+                $("#tblAdmin").append(row);
             }
-           // bindRowClickEvents()
-         //   setTextFieldValues("","","","","","","")
+           bindRowClickEvents()
+           setTextFieldValues("","","","")
         }
     });
 
 }
-//
-// function bindRowClickEvents() {
-//     $("#tblDriver>tr").click(function () {
-//         let id = $(this).children(":eq(0)").text();
-//         let name = $(this).children(":eq(1)").text();
-//         let nic = $(this).children(":eq(2)").text();
-//         let address = $(this).children(":eq(3)").text();
-//         let licen = $(this).children(":eq(4)").text();
-//         let dateOfBirth = $(this).children(":eq(5)").text();
-//         let state = $(this).children(":eq(6)").text();
-//
-//         $("#did").val(id);
-//         $("#dname").val(name);
-//         $("#dnic").val(nic);
-//         $("#daddress").val(address);
-//         $("#dlicen").val(licen);
-//         $("#dateOfBirth").val(dateOfBirth);
-//         $("#state").val(state);
-//
-//     });
-// }
-//
-// function setTextFieldValues(id, name, address, nic,licen,dateOfBirth,state) {
-//     $("#did").val(id);
-//     $("#dname").val(name);
-//     $("#dnic").val(nic);
-//     $("#daddress").val(address);
-//     $("#dlicen").val(licen);
-//     $("#dateOfBirth").val(dateOfBirth);
-//     $("#state").val(state);
-// }
-// $("#btnDriverDelete").click(function () {
-//     let id = $("#did").val();
-//     $.ajax({
-//         url: baseURL+"driver?id=" + id + "",
-//         method: "delete",
-//         dataType:"json",
-//         success: function (resp) {
-//             alert(resp.message);
-//             loadAllDrivers();
-//         },
-//         error:function (error){
-//             alert(JSON.parse(error.responseText).message);
-//         }
-//     });
-// });
-//
-// $("#btnDriverUpdate").click(function () {
-//
-//     let id=  $("#did").val();
-//     let dname=  $("#dname").val();
-//     let dnic=  $("#dnic").val();
-//     let daddress=  $("#daddress").val();
-//     let dlicen=  $("#dlicen").val();
-//     let dob=  $("#dateOfBirth").val();
-//     let sate=  $("#state").val();
-//
-//     let driver={
-//         driverID: id,
-//         name: dname,
-//         nic: dnic,
-//         address: daddress,
-//         drivingLicense: dlicen,
-//         dob: dob,
-//         status: sate
-//     }
-//
-//     $.ajax({
-//         url: baseURL+'driver',
-//         method: 'put',
-//         contentType:"application/json",
-//         data:JSON.stringify(driver),
-//         dataType:"json",
-//         success: function (res) {
-//             alert(res.message);
-//             loadAllDrivers();
-//         },
-//         error:function (error){
-//             let cause= JSON.parse(error.responseText).message;
-//             alert(cause);
-//         }
-//
-//     });
-// });
+
+function bindRowClickEvents() {
+    $("#tblAdmin>tr").click(function () {
+        let id = $(this).children(":eq(0)").text();
+        let email = $(this).children(":eq(1)").text();
+        let username = $(this).children(":eq(2)").text();
+        let password = $(this).children(":eq(3)").text();
+
+        $("#txtAdminId").val(id);
+        $("#txtAdminEmail").val(email);
+        $("#txtAdminUserName").val(username);
+        $("#txtAdminPassword").val(password);
+
+    });
+}
+
+function setTextFieldValues(id, email, username, password) {
+
+    $("#txtAdminId").val(id);
+    $("#txtAdminEmail").val(email);
+    $("#txtAdminUserName").val(username);
+    $("#txtAdminPassword").val(password);
+}
+$("#btnAdminDelete").click(function () {
+    let id = $("#txtAdminId").val();
+    $.ajax({
+        url: baseURL+"admin?id=" + id + "",
+        method: "delete",
+        dataType:"json",
+        success: function (resp) {
+            alert(resp.message);
+            loadAllAdmins();
+        },
+        error:function (error){
+            alert(JSON.parse(error.responseText).message);
+        }
+    });
+});
+
+$("#btnAdminUpdate").click(function () {
+
+    let id = $("#txtAdminId").val();
+    let email = $("#txtAdminEmail").val();
+    let username = $("#txtAdminUserName").val();
+    let password = $("#txtAdminPassword").val();
+
+    let admin={
+        adminId: id,
+        email: email,
+        username: username,
+        password: password,
+
+    }
+
+    $.ajax({
+        url: baseURL+'admin',
+        method: 'put',
+        contentType:"application/json",
+        data:JSON.stringify(admin),
+        dataType:"json",
+        success: function (res) {
+            alert(res.message);
+            loadAllDrivers();
+        },
+        error:function (error){
+            let cause= JSON.parse(error.responseText).message;
+            alert(cause);
+        }
+
+    });
+});
